22e48d1a26a10908340d6dd060ad397d77538265
NTSP3 <ntos32bit@gmail.com>
2025-06-14 20:08:41 +0530

    use ctk not ct

--- source/ai_teacher/gui.py
+++ source/ai_teacher/gui.py
@@ -5,21 +5,21 @@
 
 # ---[ Libraries ]--- #
 from ai_teacher import function as f
-import customtkinter as ct
+import customtkinter as ctk
 from tkinter import messagebox
 
-def init(title: str = "Remeny AI Teacher", width: int = 800, height: int = 600) -> "ct.CTk":
+def init(title: str = "Remeny AI Teacher", width: int = 800, height: int = 600) -> "ctk.CTk":
     """
     Initialize a window
     """
-    ct.set_appearance_mode("system")
-    win = ct.CTk()
+    ctk.set_appearance_mode("system")
+    win = ctk.CTk()
     win.geometry(f"{width}x{height}")
     win.iconbitmap("resources/icon.ico")
     win.resizable(True, True)
     return reinit(win, title)
 
-def reinit(win: "ct.CTk", title: str = "Remeny AI Teacher") -> "ct.CTk":
+def reinit(win: "ctk.CTk", title: str = "Remeny AI Teacher") -> "ctk.CTk":
     """
     Reinitialize a window
     """
@@ -37,7 +37,7 @@ def error(msg: str) -> None:
     """
     messagebox.showerror("Error", "An error occured: " + msg)
 
-def clear_window(win: "ct.CTk") -> None:
+def clear_window(win: "ctk.CTk") -> None:
     """
     Clears all stuff inside a window
     """
@@ -45,28 +45,28 @@ def clear_window(win: "ct.CTk") -> None:
         widget.destroy()
     f.dbg("Cleared window")
     
-def banner(win: "ct.CTk", heading: str, text: str, height: int = 65) -> None:
+def banner(win: "ctk.CTk", heading: str, text: str, height: int = 65) -> None:
     """
     Adds some user-friendly text at the top, expanding horizontally with window resize.
     """
     win.grid_columnconfigure(0, weight=1)
 
-    frame = ct.CTkFrame(master=win, height=height)
+    frame = ctk.CTkFrame(master=win, height=height)
     frame.grid(row=0, column=0, padx=5, pady=5, sticky="ew")
     frame.pack_propagate(False)  # Prevent content from shrinking the frame
 
-    label_heading = ct.CTkLabel(
+    label_heading = ctk.CTkLabel(
         master=frame,
         text=heading,
-        font=ct.CTkFont(size=20, weight="bold"),
+        font=ctk.CTkFont(size=20, weight="bold"),
         justify="left"
     )
     label_heading.pack(anchor="w", padx=14, pady=(10, 0))  # Top padding only
 
-    label_text = ct.CTkLabel(
+    label_text = ctk.CTkLabel(
         master=frame,
         text=text,
-        font=ct.CTkFont(size=14),
+        font=ctk.CTkFont(size=14),
         justify="left"
     )
     label_text.pack(anchor="w", padx=16, pady=(0, 5))  # Bottom padding only
